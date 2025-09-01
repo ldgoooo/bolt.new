7624cb5769356e83bd6e6b295a7da4621cc7e387
liudongguang <ldgoooo@gmail.com>
2025-09-01 12:10:45 +0800

    add

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,16 +1,20 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
 
-let api_key="sk-Xh5EGosvEgwufnBS6a2fEdF60d764295903bBf6f837088Bb"
-let baseURL="https://api.mjdjourney.cn/v1"
+const api_key = 'sk-Xh5EGosvEgwufnBS6a2fEdF60d764295903bBf6f837088Bb';
+const baseURL = 'https://api.mjdjourney.cn/v1';
+
 // let model_name="claude-3-7-sonnet-20250219"
 
-let model_name="claude-3-5-sonnet-20241022"
+// let model_name="claude-3-5-sonnet-20241022"
+
+const model_name = 'claude-sonnet-4-20250514';
+
 export function getAnthropicModel(apiKey: string) {
   const anthropic = createAnthropic({
-    apiKey:api_key,
-    baseURL: baseURL, // 代理地址
+    apiKey: api_key,
+    baseURL, // 代理地址
   });
 
   // return anthropic('claude-3-5-sonnet-20240620');
-  return anthropic(model_name)
+  return anthropic(model_name);
 }
